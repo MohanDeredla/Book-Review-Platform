85f81196336e2eb34e4602cd1b25964a5fa4aad9
poornaderedla <durgareddy.deredla@sasi.ac.in>
2025-07-23 14:00:09 +0530

    Review Entity Created

---
+++ BookReviewApplication/src/main/java/com/example/BookReviewApplication/Models/Review.java
@@ -0,0 +1,46 @@
+package com.example.BookReviewApplication.Models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String reviewText;
+	private int rating;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getReviewText() {
+		return reviewText;
+	}
+	public void setReviewText(String reviewText) {
+		this.reviewText = reviewText;
+	}
+	public int getRating() {
+		return rating;
+	}
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+	@Override
+	public String toString() {
+		return "Review [id=" + id + ", reviewText=" + reviewText + ", rating=" + rating + "]";
+	}
+	
+	
+}
